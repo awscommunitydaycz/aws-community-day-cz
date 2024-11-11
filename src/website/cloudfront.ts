4c3a727d44d72c78ee8d9b9a3f4ae4bd607ec0c4
Malanius Privierre <malaniusprivierre@gmail.com>
2024-11-11 21:11:10 +0100

    refactor(infra): extract cloudfront distribution to own construct

---
+++ src/website/cloudfront.ts
@@ -0,0 +1,80 @@
+import { CfnOutput, Duration } from "aws-cdk-lib";
+import { DnsValidatedCertificate } from "aws-cdk-lib/aws-certificatemanager";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import { S3BucketOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { IHostedZone } from "aws-cdk-lib/aws-route53";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+import path from "path";
+
+export interface WebsiteCloudFrontProps {
+  websiteBucket: Bucket;
+  hostedZone: IHostedZone;
+}
+
+export class WebsiteCloudFront extends Construct {
+  public readonly distribution: cloudfront.Distribution;
+  constructor(scope: Construct, id: string, props: WebsiteCloudFrontProps) {
+    super(scope, id);
+
+    const { websiteBucket, hostedZone } = props;
+    const domainName = hostedZone.zoneName;
+
+    const certificate = new DnsValidatedCertificate(this, "Certificate", {
+      domainName: domainName,
+      subjectAlternativeNames: [`www.${domainName}`],
+      hostedZone,
+      region: "us-east-1",
+    });
+
+    this.distribution = new cloudfront.Distribution(
+      this,
+      "WebsiteDistribution",
+      {
+        domainNames: [domainName, `www.${domainName}`],
+        certificate,
+        defaultRootObject: "index1.html",
+        defaultBehavior: {
+          origin: S3BucketOrigin.withOriginAccessControl(websiteBucket),
+          cachePolicy: new cloudfront.CachePolicy(this, "CachePolicy", {
+            enableAcceptEncodingBrotli: true,
+            enableAcceptEncodingGzip: true,
+            queryStringBehavior: cloudfront.CacheQueryStringBehavior.all(),
+            defaultTtl: Duration.days(1),
+            maxTtl: Duration.days(365),
+          }),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          functionAssociations: [
+            {
+              eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
+              function: new cloudfront.Function(this, "RedirectFunction", {
+                code: cloudfront.FunctionCode.fromFile({
+                  filePath: path.join(__dirname, "cloudfront-redirect.js"),
+                }),
+              }),
+            },
+          ],
+        },
+        errorResponses: [
+          {
+            httpStatus: 403,
+            responseHttpStatus: 403,
+            responsePagePath: "/403.html",
+            ttl: Duration.minutes(1),
+          },
+          {
+            httpStatus: 404,
+            responseHttpStatus: 404,
+            responsePagePath: "/404.html",
+            ttl: Duration.minutes(1),
+          },
+        ],
+      }
+    );
+
+    new CfnOutput(this, "WebsiteDistributionId", {
+      value: this.distribution.distributionId,
+    });
+  }
+}
--- src/website/website.stack.ts
+++ src/website/website.stack.ts
@@ -1,13 +1,11 @@
 import * as cdk from "aws-cdk-lib";
-import * as acm from "aws-cdk-lib/aws-certificatemanager";
-import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
-import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as route53 from "aws-cdk-lib/aws-route53";
 import * as route53Targets from "aws-cdk-lib/aws-route53-targets";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
 import path from "path";
+import { WebsiteCloudFront } from "./cloudfront";
 
 export interface WebsiteProps extends cdk.StackProps {
   domainName: string;
@@ -29,19 +27,17 @@ export class Website extends cdk.Stack {
       }
     );
 
-    const certificate = new acm.DnsValidatedCertificate(this, "Certificate", {
-      domainName: domainName,
-      subjectAlternativeNames: [`www.${domainName}`],
-      hostedZone,
-      region: "us-east-1",
-    });
-
     const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
       encryption: s3.BucketEncryption.S3_MANAGED,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       enforceSSL: true,
+    });
+
+    const cloudfront = new WebsiteCloudFront(this, "WebsiteCloudFront", {
+      websiteBucket,
+      hostedZone,
     });
 
     new s3deploy.BucketDeployment(this, "DeployNextWebsite", {
@@ -59,56 +55,10 @@ export class Website extends cdk.Stack {
       destinationBucket: websiteBucket,
     });
 
-    const distribution = new cloudfront.Distribution(
-      this,
-      "WebsiteDistribution",
-      {
-        defaultBehavior: {
-          origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
-          cachePolicy: new cloudfront.CachePolicy(this, "CachePolicy", {
-            enableAcceptEncodingBrotli: true,
-            enableAcceptEncodingGzip: true,
-            queryStringBehavior: cloudfront.CacheQueryStringBehavior.all(),
-            defaultTtl: cdk.Duration.days(1),
-            maxTtl: cdk.Duration.days(365),
-          }),
-          viewerProtocolPolicy:
-            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-          functionAssociations: [
-            {
-              eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
-              function: new cloudfront.Function(this, "RedirectFunction", {
-                code: cloudfront.FunctionCode.fromFile({
-                  filePath: path.join(__dirname, "cloudfront-redirect.js"),
-                }),
-              }),
-            },
-          ],
-        },
-        domainNames: [domainName, `www.${domainName}`],
-        certificate: certificate,
-        errorResponses: [
-          {
-            httpStatus: 403,
-            responseHttpStatus: 403,
-            responsePagePath: "/403.html",
-            ttl: cdk.Duration.minutes(1),
-          },
-          {
-            httpStatus: 404,
-            responseHttpStatus: 404,
-            responsePagePath: "/404.html",
-            ttl: cdk.Duration.minutes(1),
-          },
-        ],
-        defaultRootObject: "index1.html",
-      }
-    );
-
     new route53.ARecord(this, "WebsiteAliasRecord", {
       zone: hostedZone,
       target: route53.RecordTarget.fromAlias(
-        new route53Targets.CloudFrontTarget(distribution)
+        new route53Targets.CloudFrontTarget(cloudfront.distribution)
       ),
       recordName: domainName,
     });
@@ -116,17 +66,13 @@ export class Website extends cdk.Stack {
     new route53.ARecord(this, "WebsiteWWWAliasRecord", {
       zone: hostedZone,
       target: route53.RecordTarget.fromAlias(
-        new route53Targets.CloudFrontTarget(distribution)
+        new route53Targets.CloudFrontTarget(cloudfront.distribution)
       ),
       recordName: `www.${domainName}`,
     });
 
     new cdk.CfnOutput(this, "WebsiteURL", {
       value: `https://${domainName}`,
-    });
-
-    new cdk.CfnOutput(this, "WebsiteDistributionId", {
-      value: distribution.distributionId,
     });
   }
 }
