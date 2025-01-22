4672dfa78d656eb0da8ba729e6c3e725b60334f9
Malanius Privierre <malaniusprivierre@gmail.com>
2025-01-22 21:08:49 +0100

    feat(infra): create GH identity provider and role

---
+++ src/deploy/github-deploy.stack.ts
@@ -0,0 +1,48 @@
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+export interface GitHubDeployProps extends cdk.StackProps {
+  repository: string;
+  branch?: string;
+}
+
+export class GitHubDeploy extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: GitHubDeployProps) {
+    super(scope, id, props);
+
+    const { repository, branch } = props;
+    const branchPath = branch ? `ref:refs/heads/${branch}` : '*';
+
+    const githubProvider = new iam.OpenIdConnectProvider(
+      this,
+      'GitHubProvider',
+      {
+        url: 'https://token.actions.githubusercontent.com',
+        clientIds: ['sts.amazonaws.com'],
+      }
+    );
+
+    new iam.Role(this, 'CdkDeployRole', {
+      assumedBy: new iam.FederatedPrincipal(
+        githubProvider.openIdConnectProviderArn
+      ).withConditions({
+        StringEquals: {
+          'token.actions.githubusercontent.com:sub': `repo:${repository}:${branchPath}`,
+          'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
+        },
+      }),
+      inlinePolicies: {
+        allowCdkAssume: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: ['sts:AssumeRole'],
+              resources: ['arn:aws:iam::*:role/cdk-*'],
+            }),
+          ],
+        }),
+      },
+    });
+  }
+}
